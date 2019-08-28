77d60b18025cd34846f4aeccb2e30e3f58ab2854
Kris Jordan <krisjordan@gmail.com>
2019-08-28 00:40:49 -0400

    Remove quiet pull of images and add comp290 image

--- learncli.ps1
+++ learncli.ps1
@@ -4,5 +4,5 @@ param
     $command = "bash"
 )
 
-Invoke-Expression "docker-compose pull --quiet $command"
+Invoke-Expression "docker-compose pull $command"
 Invoke-Expression "docker-compose run --rm $command"
--- learncli.sh
+++ learncli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose pull --quiet "${1:-bash}"
+docker-compose pull "${1:-bash}"
 docker-compose run --rm "${1:-bash}"
